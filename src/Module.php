767c5011988ce54797e3bec4c63a0ab8a22f1644
Designmoves <github@designmoves.nl>
2015-03-30 23:31:07 +0200

    Updated autoloader path

--- src/Module.php
+++ src/Module.php
@@ -44,11 +44,11 @@ class Module implements
     {
         return array(
             'Zend\Loader\ClassMapAutoloader' => array(
-                __DIR__ . '/autoload_classmap.php',
+                __DIR__ . '/../autoload_classmap.php',
             ),
             'Zend\Loader\StandardAutoloader' => array(
                 'namespaces' => array(
-                    __NAMESPACE__ => __DIR__ . '/src/' . str_replace('\\', '/', __NAMESPACE__),
+                    __NAMESPACE__ => __DIR__ . '/src',
                 ),
             ),
         );
@@ -59,6 +59,6 @@ class Module implements
      */
     public function getConfig()
     {
-        return include __DIR__ . '/config/module.config.php';
+        return include __DIR__ . '/../config/module.config.php';
     }
 }
